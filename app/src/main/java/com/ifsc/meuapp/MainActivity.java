50ee02c76052c96bbe6d79147d835ae1f815e29b
Edder <egaddini.dev@gmail.com>
2023-02-19 17:39:00 -0300

    A1-E4/Em grupo de até 4 pessoas criar uma aplicação para sorteio de números aleatórios dentro de um determinado       intervalo informado pelo usuário

--- app/src/main/java/com/ifsc/meuapp/MainActivity.java
+++ app/src/main/java/com/ifsc/meuapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+
+        EditText maxValue = findViewById(R.id.maxValue);
+        EditText minValue = findViewById(R.id.minValue);
+
+        int max = Integer.parseInt(maxValue.getText().toString());
+        int min = Integer.parseInt(minValue.getText().toString());
+
+        int valor = (int) (Math.random() * (max - min +1) + min);
+
+        TextView text = findViewById(R.id.painel);
+        text.setText(Integer.toString(valor));
     }
 
 }
