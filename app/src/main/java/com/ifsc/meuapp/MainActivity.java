a1d717a01a850c0e4ea2a73b622433c8d6ccc90a
Edder <egaddini.dev@gmail.com>
2023-02-19 16:07:02 -0300

    A1-E2/Alterar sua aplicação para que conte o número de clicks sobre o texto HelloWorld!

--- app/src/main/java/com/ifsc/meuapp/MainActivity.java
+++ app/src/main/java/com/ifsc/meuapp/MainActivity.java
@@ -9,10 +9,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvHelloWorld;
+    private int counter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHelloWorld = findViewById(R.id.tvHelloWorld);
+    }
+
+    public void onClick(View view) {
+        counter ++;
+        tvHelloWorld.setText(Integer.toString(counter));
     }
 
 }
