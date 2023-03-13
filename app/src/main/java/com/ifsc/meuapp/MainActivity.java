dcff9cd159618914d3e1adc4ed00886f4179628b
Edder <egaddini.dev@gmail.com>
2023-03-12 22:07:11 -0300

    Tarefa Fragmento

--- app/src/main/java/com/ifsc/meuapp/MainActivity.java
+++ app/src/main/java/com/ifsc/meuapp/MainActivity.java
@@ -1,18 +1,59 @@
 package com.ifsc.meuapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Fragment fragment;
+    Button buttonA, buttonB, buttonC;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        buttonA.setOnClickListener(this);
+        buttonB.setOnClickListener(this);
+        buttonC.setOnClickListener(this);
+
+    }
+
+    public void abreFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout1, fragment);
+        transaction.commit();
+    }
+
+    public void abreFragmentExterno() {
+        fragment = new FragmentoA();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout1, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonA:
+                fragment = new FragmentoA();
+                break;
+            case R.id.buttonB:
+                fragment = new FragmentoB();
+                break;
+            case R.id.buttonC:
+                fragment = new FragmentoC();
+                break;
+        }
+        abreFragment();
     }
 
 }
