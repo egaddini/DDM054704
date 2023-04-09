6b7fff79c3760333de475a272ce95fa7eba86aa0
Edder <egaddini.dev@gmail.com>
2023-04-08 23:32:38 -0300

    MyPaint tarefa do paint

--- app/src/main/java/com/ifsc/meuapp/MyPaint.java
+++ app/src/main/java/com/ifsc/meuapp/MyPaint.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 
@@ -15,11 +16,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyPaint extends View {
-
-//    List<Paint> mPaints = new ArrayList<>();
-//    List<Path> mPaths = new ArrayList<>();
-    Paint mPaint;
-    Path mPath;
+    private List<Paint> mPaints = new ArrayList<>();
+    private List<Path> mPaths = new ArrayList<>();
+    private Paint mPaint;
+    private Path mPath;
 
     public MyPaint(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -28,12 +28,36 @@ public class MyPaint extends View {
         mPaint.setColor(Color.BLACK);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(5);
+        mPaints.add(mPaint);
+        mPaths.add(mPath);
+
+    }
+
+    public void addNewLayer() {
+        mPaint = new Paint();
+        mPath = new Path();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+        mPaints.add(mPaint);
+        mPaths.add(mPath);
+        invalidate();
+    }
+
+    public void setCurrentLayerColor(int color) {
+        mPaints.get(mPaints.size() - 1).setColor(color);
+    }
+
+    public void setCurrentLayerWidth(int width) {
+        mPaints.get(mPaints.size() - 1).setStrokeWidth(width);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath(mPath, mPaint);
+        for (int i = 0; i < mPaths.size(); i++) {
+            canvas.drawPath(mPaths.get(i), mPaints.get(i));
+        }
     }
 
     @Override
@@ -51,9 +75,7 @@ public class MyPaint extends View {
                 break;
             case (MotionEvent.ACTION_UP):
                 mPath.lineTo(x, y);
-                mPaint.setColor(Color.RED);
-//                mPaints.add(mPaint);
-//                mPaths.add(mPath);
+                addNewLayer();
                 break;
             default:
                 break;
